defeeb65601bf595628ed05014243b9c51e008d7
ziankork <mynameiszian@gmail.com>
2024-12-30 13:03:43 +0700

    feat: add error types

--- py-bindings/src/lib.rs
+++ py-bindings/src/lib.rs
@@ -11,31 +11,12 @@ use sim::{
     evm::Tx,
     simulator::{ExecutionError, ExecutionResult, Simulator},
 };
-use types::Transaction;
-
-static mut SIMULATOR: Option<Arc<Simulator>> = None;
-
-#[pyfunction]
-pub fn initialize(url: &str) {
-    let simulator = Simulator::new(url).unwrap();
-
-    unsafe {
-        SIMULATOR = Some(simulator.into());
-    }
-}
-
-pub fn simulator() -> &'static Arc<Simulator> {
-    unsafe { SIMULATOR.as_ref().expect("Simulator not initialized") }
-}
-
-#[pyfunction]
-pub fn simulate(txs: Vec<Transaction>){
-    // simulator().simulate(txs.into());
-}
+use types::{EvmSimulator, Transaction};
 
 #[pymodule]
 fn pyo3_example(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_class::<Transaction>()?;
-    todo!();
-    // m.add_function(wrap_pyfunction!(pyfunction, m)?)
+    m.add_class::<EvmSimulator>()?;
+
+    Ok(())
 }
--- py-bindings/src/types.rs
+++ py-bindings/src/types.rs
@@ -1,8 +1,26 @@
+use std::{result::Result, sync::Arc};
+
 use pyo3::FromPyObject;
 use sim::{
     evm::{Address, Tx, Uint256},
+    simulator::Simulator,
     utils::Utils,
 };
+
+pub type Result<T> = std::result::Result<T, GenericError<anyhow::Error>>;
+
+#[derive(Debug, Clone)]
+pub struct GenericError<T> {
+    inner: T,
+}
+
+impl<T: std::error::Error> std::error::Error for GenericError<T> {}
+
+impl<T: std::error::Error> std::fmt::Display for GenericError<T> {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(f, "Error: {}", self.inner)
+    }
+}
 
 #[pyo3::pyclass]
 #[derive(Clone, Debug)]
@@ -84,5 +102,24 @@ impl Transaction {
     #[setter]
     pub fn set_data(&mut self, data: Vec<u8>) {
         self.data = data;
+    }
+}
+
+#[derive(Clone)]
+#[pyo3::pyclass]
+pub struct EvmSimulator {
+    // we wrap it in Arc, since all simulation doesn't require mutable access
+    // pyo3 require the struct to be cloneable but since we don't actually have to
+    // we can just wrap it in Arc to minimize copying data around
+    inner: Arc<Simulator>,
+}
+
+#[pyo3::pymethods]
+impl EvmSimulator {
+    #[new]
+    pub fn new(url: &str) -> Result<Self> {
+        let inner = Simulator::new(url)?.into();
+
+        Ok(Self { inner })
     }
 }
