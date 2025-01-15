5d1de077869960bce14ff281a69de0010f3c5bdc
ziankork <mynameiszian@gmail.com>
2025-01-15 14:52:19 +0000

    feat: add pyproject

--- py-bindings/src/lib.rs
+++ py-bindings/src/lib.rs
@@ -11,12 +11,20 @@ use sim::{
     evm::Tx,
     simulator::{ExecutionError, ExecutionResult, Simulator},
 };
-use types::{EvmSimulator, Transaction};
+use types::{
+    DatabaseErrorRef, EvmExecutionError, EvmExecutionResult, EvmSimulator, SimulationResult,
+    Transaction, Txs,
+};
 
 #[pymodule]
-fn pyo3_example(m: &Bound<'_, PyModule>) -> PyResult<()> {
+fn evm_sim(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_class::<Transaction>()?;
     m.add_class::<EvmSimulator>()?;
+    m.add_class::<Txs>()?;
+    m.add_class::<EvmExecutionResult>()?;
+    m.add_class::<SimulationResult>()?;
+    m.add_class::<DatabaseErrorRef>()?;
+    m.add_class::<EvmExecutionError>()?;
 
     Ok(())
 }
