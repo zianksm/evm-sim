bd40e37e3b98429ca999f849cf0840d75b1b2ae9
zianksm <mynameiszian@gmail.com>
2025-02-18 16:29:55 +0800

    partially implement grpc server

---
+++ rpc/src/main.rs
@@ -0,0 +1,162 @@
+use std::{collections::HashMap, sync::Arc};
+
+use sim::{evm_primitives::{self, ExecutionResult}, simulator::ExecutionResult, utils::Utils};
+use simulator::simulator_server::SimulatorServer;
+use tokio::sync::RwLock;
+use tonic::{server, transport::Server, Request, Response, Status};
+
+pub mod simulator {
+    tonic::include_proto!("simulator");
+}
+
+pub type ChainId = u32;
+pub struct Simulators {
+    servers: RwLock<HashMap<ChainId, sim::simulator::Simulator>>,
+}
+
+impl Simulators {
+    pub fn new() -> Self {
+        Self {
+            servers: RwLock::new(HashMap::new()),
+        }
+    }
+
+    pub async fn initialize(&self, chain_id: ChainId, url: &str) -> Result<(), Status> {
+        let mut servers = self.servers.write().await;
+
+        if servers.contains_key(&chain_id) {
+            return Err(Status::already_exists("Chain already initialized"));
+        }
+
+        let sim =
+            sim::simulator::Simulator::new(url).map_err(|e| Status::internal(e.to_string()))?;
+
+        servers.insert(chain_id, sim);
+
+        Ok(())
+    }
+}
+
+impl From<ExecutionResult> for simulator::Result {
+    fn from(value: ExecutionResult) -> Self {
+        match value {
+            ExecutionResult::Success {
+                reason,
+                gas_used,
+                gas_refunded,
+                logs,
+                output,
+            } => Self {
+                is_success: true,
+                revert: None,
+                success: Some(simulator::Success {
+                    reason: serde_json::to_string_pretty(&reason).expect("internal serialization should not fail"),
+                    gas_used,
+                    gas_refunded,
+                    logs,
+                    output,
+                }),
+            },
+            ExecutionResult::Revert { gas_used, output } => todo!(),
+            ExecutionResult::Halt { reason, gas_used } => todo!(),
+        }
+    }
+}
+
+
+impl From<evm_primitives::Log> for simulator::Log {
+    fn from(value: evm_primitives::Log) -> Self {
+        Self {
+            address: value.address.to_string(),
+            topics: value.topics.iter().map(|t| t.to_string()).collect(),
+            data: value.data.to_string(),
+        }
+    }
+}
+
+#[tonic::async_trait]
+impl simulator::simulator_server::Simulator for Simulators {
+    async fn simulate(
+        &self,
+        request: Request<simulator::Transactions>,
+    ) -> Result<Response<simulator::Results>, Status> {
+        let req = request.into_inner();
+        let chain_id = req.chain_id;
+        let txs = req
+            .transactions
+            .into_iter()
+            .map(TryFrom::try_from)
+            .collect::<Result<Vec<_>, _>>()?;
+        let servers = self.servers.read().await;
+
+        let sim = servers
+            .get(&chain_id)
+            .ok_or_else(|| Status::not_found("No simulator found"))?;
+
+        let results = sim.simulate(txs);
+
+        for result in results {
+            match result {
+                Ok(result) => {}
+                Err(e) => {
+                    return Err(Status::internal(e.to_string()));
+                }
+            }
+        }
+
+        todo!()
+    }
+
+    // TODO: add caches for urls
+    async fn initialize(
+        &self,
+        request: Request<simulator::InitializeRequest>,
+    ) -> Result<Response<simulator::InitializeReply>, Status> {
+        let mut servers = self.servers.write().await;
+        let req = request.into_inner();
+
+        if servers.contains_key(&req.chain_id) {
+            return Err(Status::already_exists("Chain already initialized"));
+        }
+
+        let sim = sim::simulator::Simulator::new(&req.url)
+            .map_err(|e| Status::internal(e.to_string()))?;
+
+        servers.insert(req.chain_id, sim);
+
+        Ok(Response::new(simulator::InitializeReply {
+            message: "Initialized".into(),
+        }))
+    }
+}
+
+impl TryFrom<simulator::Transaction> for sim::evm::Tx {
+    type Error = tonic::Status;
+
+    fn try_from(value: simulator::Transaction) -> Result<Self, Self::Error> {
+        Ok(Self {
+            data: hex::decode(value.hex_data)
+                .map_err(|_| Status::invalid_argument("Invalid data"))?,
+            from: Utils::address_try_from_string(value.from)
+                .map_err(|_| Status::invalid_argument("Invalid from"))?,
+            to: Utils::address_try_from_string(value.to)
+                .map_err(|_| Status::invalid_argument("Invalid to"))?,
+            value: Utils::uint_try_from_string(&value.value)
+                .map_err(|_| Status::invalid_argument("Invalid value"))?,
+        })
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let addr = "[::1]:50051".parse()?;
+
+    let sim = Simulators::new();
+
+    Server::builder()
+        .add_service(SimulatorServer::new(sim))
+        .serve(addr)
+        .await?;
+
+    Ok(())
+}
